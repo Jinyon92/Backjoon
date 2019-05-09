018f93b95c0b3bf4466596ee0648412df2a97105
choijinhyun <chjh0902@naver.com>
2019-05-09 21:46:34 +0900

    (logN)finish

--- Summercoding2017_jump_and_moving/Summercoding2017_jump_and_moving/main.cpp
+++ Summercoding2017_jump_and_moving/Summercoding2017_jump_and_moving/main.cpp
@@ -1,7 +1,18 @@
-#include<iostream>
+
+#include <iostream>
 #include<cmath>
 using namespace std;
 
-int main()
+int solution(int n)
 {
-    
+    int ans = 0;
+    while(n != 0){
+        if(n%2 == 0){
+            n /= 2;
+        }else{
+            ans += 1;
+            n /= 2;
+        }
+    }
+    return ans;
+}
