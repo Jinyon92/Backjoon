abb98e80e7a233cee1b95219b7d6964c5e32a60b
choijinhyun <chjh0902@naver.com>
2019-10-09 10:41:33 +0900

    2*n 타일2 (다이나믹프로그래밍) finish

---
+++ Backjoon11727/Backjoon11727/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int dp[1001];
+
+int main()
+{
+    int n;
+    cin>>n;
+    dp[1] = 1;
+    dp[2] = 3;
+    for(int i=3; i<=n; i++){
+        dp[i] = (dp[i-1] + 2*dp[i-2]) % 10007;
+    }
+    cout<<dp[n];
+    return 0;
+}
